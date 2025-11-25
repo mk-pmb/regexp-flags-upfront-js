3d3c4c5843beb1bc012acad53e144c40cafe2ff1
mk-pmb <mk-pmb@users.noreply.github.com>
2025-11-25 21:26:32 +0100

    v0.1.2: Adjust code to eslint rules rather than jslint.

--- rfu.js
+++ rfu.js
@@ -1,11 +1,8 @@
-/* -*- coding: UTF-8, tab-width: 2 -*- */
-// eslint-disable-next-line spaced-comment
-/*jslint indent: 2, maxlen: 80, browser: true */
-/*globals define:true*/
+'use strict';
 (function setup() {
-  'use strict';
-  var EX = function makeRegExp(spec, addFlags) {
-    var p = spec, f = (EX.flagsRx.exec(spec) || '');
+  const EX = function makeRegExp(spec, addFlags) {
+    let p = spec;
+    let f = (EX.flagsRx.exec(spec) || '');
     if (f) {
       p = p.slice(f[0].length);
       f = f[1];
@@ -25,11 +22,11 @@
 
   EX.flagsRx = /^\(\?(\w*):\)/;
 
-  (function (e) { // alternate export
-    /*global define: true */
-    var d = ((typeof define === 'function') && define),
-      m = ((typeof module === 'object') && module);
-    if (d && d.amd) { d(function () { return e; }); }
+  (function universalExport(e) {
+    /* globals define:true */
+    const d = ((typeof define === 'function') && define);
+    const m = ((typeof module === 'object') && module);
+    if (d && d.amd) { d(function amdFactory() { return e; }); }
     if (m && m.exports) { m.exports = e; }
   }(EX));
 }());
